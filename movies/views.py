19e631e8aaaafdd21b8346bae1739302387f8e0f
farrelpratama <farrepratama512@gmail.com>
2025-12-19 01:52:50 +0700

    add random and bookmark-based movie recommendations to home page

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,48 @@
+import random
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .services import search_movies, get_movie_detail
 from django.contrib.auth.decorators import login_required
 from reviews.models import Review
 from .models import Bookmark
+from .services import search_movies, get_movie_detail
 
 def home(request):
-    recommendations = search_movies("avengers")
+    # =========================
+    # RANDOM RECOMMENDATION
+    # =========================
+    random_data = search_movies("movie")
+    random_movies = []
+
+    if random_data and random_data.get("Response") == "True":
+        random_movies = random_data.get("Search", [])
+        random.shuffle(random_movies)
+        random_movies = random_movies[:8]
+
+    # =========================
+    # BOOKMARK-BASED RECOMMENDATION
+    # =========================
+    bookmark_movies = []
+
+    if request.user.is_authenticated:
+        bookmarks = Bookmark.objects.filter(user=request.user)
+
+        keywords = set()
+        for b in bookmarks:
+            # Ambil kata pertama judul (simple & aman)
+            keywords.add(b.title.split()[0])
+
+        for keyword in keywords:
+            data = search_movies(keyword)
+            if data and data.get("Response") == "True":
+                bookmark_movies.extend(data.get("Search", []))
+
+        random.shuffle(bookmark_movies)
+        bookmark_movies = bookmark_movies[:8]
+
     return render(request, "movies/home.html", {
-        "recommendations": recommendations
+        "random_movies": random_movies,
+        "bookmark_movies": bookmark_movies
     })
 
 def search(request):
